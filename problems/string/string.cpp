8e9f4207a3591116fa233ebd876d8e45e7f1fa57
Denis Stepulenok <super.denis@gmail.com>
2015-12-10 00:54:56 +0300

    Save: DSTEPULE-RU

--- problems/string/string.cpp
+++ problems/string/string.cpp
@@ -9,10 +9,8 @@ int main() {
   freopen("string.out", "w", stdout);
   string a;
   getline(cin, a);
-
   for(int i = a.length() - 1; i >= 0; --i)
     cout << a[i];
-
   cout << endl;
   return 0;
 }
